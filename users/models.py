f97499151a39da6282bf3633463f7175bc6a4b43
Killerulya2015 <Killerulya2015@yandex.ru>
2025-09-18 14:34:46 +0000

    username fixed

--- users/models.py
+++ users/models.py
@@ -1,17 +1,29 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
     """
-    Custom user model.
+    Custom user model that uses 'username' for login but requires a unique email.
+    This inherits all fields from AbstractUser (including username, password, email, etc.)
     """
-    email = models.EmailField(unique=True, verbose_name='Email')
+    # By default, AbstractUser has USERNAME_FIELD = 'username'. We will stick to that.
+    # We are overriding the email field to make it required and unique.
+    email = models.EmailField(
+        unique=True,
+        verbose_name='Email',
+        help_text='Required. Enter a valid email address.'
+    )
+
+    # These fields are optional additions to the user model
     phone = models.CharField(max_length=35, verbose_name='Phone', blank=True, null=True)
     avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Avatar', blank=True, null=True)
     country = models.CharField(max_length=100, verbose_name='Country', blank=True, null=True)
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+
+    # REQUIRED_FIELDS are fields prompted for when creating a superuser.
+    # 'username' and 'password' are required by default.
+    # We are adding 'email' to this list.
+    REQUIRED_FIELDS = ['email']
 
     class Meta:
         verbose_name = 'User'
